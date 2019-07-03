b003bbd9df991f60d18c1d639a75e7e72c39c1b0
Narrakas <netkas@users.noreply.github.com>
2019-07-03 12:09:13 -0400

    Added missing resources

---
+++ Netlenium/Netlenium.Driver/Chrome/DriverManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netlenium.Driver.Chrome
+{
+    public class DriverManager : IDriverManager
+    {
+    }
+}

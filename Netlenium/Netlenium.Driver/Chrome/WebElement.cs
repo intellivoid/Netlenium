918d5004d68c7500f2b97e2f6d163949ae4a71ec
Narrakas <netkas@users.noreply.github.com>
2019-07-06 23:57:16 -0400

    Added GetAttribute and SetAttribute to Netlenium.Driver.Chrome.WebElement

--- Netlenium/Netlenium.Driver/Chrome/WebElement.cs
+++ Netlenium/Netlenium.Driver/Chrome/WebElement.cs
@@ -22,11 +22,26 @@ namespace Netlenium.Driver.Chrome
             this.driver = driver;
         }
 
+        public string GetAttribute(string attributeName)
+        {
+            driver.Logging.WriteEntry(Logging.MessageType.Information, $"IWebElement[{webElement.ToString()}]", $"Getting value of attribute '{attributeName}'");
+            var value = webElement.GetAttribute(attributeName);
+            driver.Logging.WriteEntry(Logging.MessageType.Debugging, $"IWebElement[{webElement.ToString()}]", $"Returned value '{attributeName}'");
+            return value;
+        }
+
         public void SendKeys(string input)
         {
             driver.Logging.WriteEntry(Logging.MessageType.Information, $"IWebElement[{webElement.ToString()}]", $"Entering '{input}' to element");
             webElement.SendKeys(input);
             driver.Logging.WriteEntry(Logging.MessageType.Verbose, $"IWebElement[{webElement.ToString()}]", "Operation completed");
+        }
+
+        public void SetAttribute(string attributeName, string value)
+        {
+            driver.Logging.WriteEntry(Logging.MessageType.Information, $"IWebElement[{webElement.ToString()}]", $"Setting value of attribute '{attributeName}' to ''{value}");
+            driver.Logging.WriteEntry(Logging.MessageType.Debugging, $"IWebElement[{webElement.ToString()}]", $"Executing eval; arguments[0].setAttribute(arguments[{attributeName}], arguments[{value}]);");
+            driver.RemoteDriver.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2]);", webElement, attributeName, value);
         }
     }
 }

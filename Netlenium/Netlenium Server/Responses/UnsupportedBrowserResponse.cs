8d33095763941fddd3f1343124cb609dc6b5c9df
Narrakas <netkas@users.noreply.github.com>
2019-07-09 22:22:59 -0400

    Added missing docs for Netlenium.Server.Responses.UnsupportedBrowserResponse

--- Netlenium/Netlenium Server/Responses/UnsupportedBrowserResponse.cs
+++ Netlenium/Netlenium Server/Responses/UnsupportedBrowserResponse.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetleniumServer.Responses
 {
+    /// <summary>
+    /// A response when the given target browser is unsupported
+    /// </summary>
     [Serializable]
     public class UnsupportedBrowserResponse : IResponse
 
@@ -16,6 +15,9 @@ namespace NetleniumServer.Responses
 
         public string Message { get; set; }
 
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
         public UnsupportedBrowserResponse()
         {
             Status = false;

ba865ebdb68c2f4e70b1cc9bf0af7ef8b5a23def
Narrakas <netkas@users.noreply.github.com>
2019-07-09 22:23:28 -0400

    Made NetleniumServer.Responses.UnsupportedRequestMethodResponse a serializable object

--- Netlenium/Netlenium Server/Responses/UnsupportedRequestMethodResponse.cs
+++ Netlenium/Netlenium Server/Responses/UnsupportedRequestMethodResponse.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetleniumServer.Responses
 {
+    [Serializable]
     public class UnsupportedRequestMethodResponse : IResponse
     {
         public bool Status { get; set; }

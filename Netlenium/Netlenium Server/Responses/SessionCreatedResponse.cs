46c9c88570a8eebeaa0a74dd21030f149bd33512
Narrakas <netkas@users.noreply.github.com>
2019-07-09 22:20:27 -0400

    Made NetleniumServer.Responses.SessionCreatedResponse a serializable object

--- Netlenium/Netlenium Server/Responses/SessionCreatedResponse.cs
+++ Netlenium/Netlenium Server/Responses/SessionCreatedResponse.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetleniumServer.Responses
 {
+    [Serializable]
     public class SessionCreatedResponse : IResponse
     {
         public bool Status { get; set; }

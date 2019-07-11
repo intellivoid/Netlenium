cb4ad0ecfa20ed852b01f31054f0eca61bcb7475
Narrakas <netkas@users.noreply.github.com>
2019-07-11 17:28:01 -0400

    Created NetleniumServer.Objects.Size

---
+++ Netlenium/Netlenium Server/Objects/Size.cs
@@ -0,0 +1,12 @@
+﻿using System
+
+namespace NetleniumServer.Objects
+{
+    [Serializable]
+    public class Size
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+    }
+}

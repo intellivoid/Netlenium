ee28d04981ee7f48ced68c47b337b726b062b38a
Narrakas <netkas@users.noreply.github.com>
2019-07-10 20:10:21 -0400

    Created NetleniumServer.Responses.SessionErrorResponse

---
+++ Netlenium/Netlenium Server/Responses/SessionErrorResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given when there is a session error
+    /// </summary>
+    [Serializable]
+    public class SessionErrorResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="error"></param>
+        public SessionErrorResponse(string error)
+        {
+            Status = true;
+            ResponseCode = 500;
+            Message = error;
+        }
+    }
+}

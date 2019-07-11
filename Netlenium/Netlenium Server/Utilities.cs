103a83a2099440a17760a8266accdbbda4e4fc42
Narrakas <netkas@users.noreply.github.com>
2019-07-11 18:41:34 -0400

    Added ParseSearchBy() to NetleniumServer.Utilities

---
+++ Netlenium/Netlenium Server/Utilities.cs
@@ -0,0 +1,42 @@
+﻿using Netlenium.Driver;
+
+namespace NetleniumServer
+{
+    /// <summary>
+    /// General Utilities Class
+    /// </summary>
+    public static class Utilities
+    {
+        /// <summary>
+        /// Parses the SearchBy value, throws an exception if it's invalid
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static SearchBy ParseSearchBy(string input)
+        {
+            switch (input.ToLower())
+            {
+                case "class_name":
+                    return SearchBy.ClassName;
+
+                case "css_selector":
+                    return SearchBy.CssSelector;
+
+                case "id":
+                    return SearchBy.Id;
+
+                case "name":
+                    return SearchBy.Name;
+
+                case "tag_name":
+                    return SearchBy.TagName;
+
+                case "xpath":
+                    return SearchBy.XPath;
+
+                default:
+                    throw new InvalidSearchByValueException();
+            }
+        }
+    }
+}

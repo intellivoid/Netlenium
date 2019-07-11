ee9e28b933b93f3dfcf58cd55919cbd628943be1
Narrakas <netkas@users.noreply.github.com>
2019-07-11 19:06:05 -0400

    Added NetleniumServer.Responses.RequestSuccessResponse

---
+++ Netlenium/Netlenium Server/Responses/RequestSuccessResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given when the given request was a success
+    /// </summary>
+    [Serializable]
+    public class RequestSuccessResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public RequestSuccessResponse()
+        {
+            Status = true;
+            ResponseCode = 200;
+        }
+    }
+}

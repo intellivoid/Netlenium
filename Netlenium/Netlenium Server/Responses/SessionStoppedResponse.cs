ff2f613bc8740c65858d85354cad4b3223f0a610
Narrakas <netkas@users.noreply.github.com>
2019-07-10 20:06:41 -0400

    Created NetleniumServer.Responses.SessionStoppedResponse

---
+++ Netlenium/Netlenium Server/Responses/SessionStoppedResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given when the Session was stopped without any errors
+    /// </summary>
+    [Serializable]
+    public class SessionStoppedResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public SessionStoppedResponse()
+        {
+            Status = true;
+            ResponseCode = 200;
+        }
+    }
+}

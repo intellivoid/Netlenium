c6ec64c1615b7d7bdecc3e29f71b4d05c44607ad
Narrakas <netkas@users.noreply.github.com>
2019-07-11 18:37:45 -0400

    Created UnexpectedErrorResponse

---
+++ Netlenium/Netlenium Server/Responses/UnexpectedErrorResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given when there was an unexpected error
+    /// </summary>
+    [Serializable]
+    public class UnexpectedErrorResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="exceptionMessage"></param>
+        public UnexpectedErrorResponse(string exceptionMessage)
+        {
+            Status = false;
+            ResponseCode = 500;
+            Message = exceptionMessage;
+        }
+    }
+}

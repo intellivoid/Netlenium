ee9c6690e8b0b15acd50ddf9661f5bd3a4637160
Narrakas <netkas@users.noreply.github.com>
2019-07-10 20:01:35 -0400

    Created NetleniumServer.Responses.SessionNotFoundResponse

---
+++ Netlenium/Netlenium Server/Responses/SessionNotFoundResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given when the Session was not found
+    /// </summary>
+    [Serializable]
+    public class SessionNotFoundResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="sessionId"></param>
+        public SessionNotFoundResponse(string sessionId)
+        {
+            Status = false;
+            ResponseCode = 404;
+            Message = $"The session '{sessionId}' was not found";
+        }
+    }
+}

915947bd0d355eac79b026514836b3312d1a4cfb
Narrakas <netkas@users.noreply.github.com>
2019-07-11 17:29:15 -0400

    Added missing public constructor

--- Netlenium/Netlenium Server/Objects/Size.cs
+++ Netlenium/Netlenium Server/Objects/Size.cs
@@ -1,4 +1,4 @@
-﻿using System
+﻿using System;
 
 namespace NetleniumServer.Objects
 {
@@ -8,5 +8,15 @@ namespace NetleniumServer.Objects
         public int Width { get; set; }
 
         public int Height { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="size"></param>
+        public Size(Size size)
+        {
+            Width = size.Width;
+            Height = size.Height;
+        }
     }
 }

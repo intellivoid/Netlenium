b4773c4a882194e798d01b19b1e2137f6a1bb960
Narrakas <netkas@users.noreply.github.com>
2019-07-11 21:04:41 -0400

    Created Netlenium.Driver.WindowHandleException

---
+++ Netlenium/Netlenium.Driver/WindowHandleException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Netlenium.Driver
+{
+    [Serializable]
+    internal class WindowHandleException : Exception
+    {
+        public WindowHandleException()
+        {
+        }
+
+        public WindowHandleException(string message) : base(message)
+        {
+        }
+
+        public WindowHandleException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected WindowHandleException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

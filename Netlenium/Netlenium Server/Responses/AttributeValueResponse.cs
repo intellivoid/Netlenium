03ae7ff809ed3ddf90bc47335e77940f2890ca94
Narrakas <netkas@users.noreply.github.com>
2019-07-13 14:38:23 -0400

    Created AttributeValueResponse in NetleniumServer.Responses

---
+++ Netlenium/Netlenium Server/Responses/AttributeValueResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given for the value of an attribute from a element
+    /// </summary>
+    [Serializable]
+    public class AttributeValueResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string AttributeValue { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="attributeValue"></param>
+        public AttributeValueResponse(string attributeValue)
+        {
+            Status = true;
+            ResponseCode = 200;
+            AttributeValue = attributeValue;
+        }
+    }
+}

6668c09017854c2b424aed346fc6c8bcde9f432d
Narrakas <netkas@users.noreply.github.com>
2019-07-13 00:44:08 -0400

    Created ElementNotSelectableException in Netlenium.Driver

---
+++ Netlenium/Netlenium.Driver/ElementNotSelectableException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Netlenium.Driver
+{
+    [Serializable]
+    internal class ElementNotSelectableException : Exception
+    {
+        public ElementNotSelectableException()
+        {
+        }
+
+        public ElementNotSelectableException(string message) : base(message)
+        {
+        }
+
+        public ElementNotSelectableException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ElementNotSelectableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

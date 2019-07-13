533770c02c1a15b8828c583cf84b8d32d4e21ef3
Narrakas <netkas@users.noreply.github.com>
2019-07-13 00:43:20 -0400

    Created ElementNotInteractableException in Netlenium.Driver

---
+++ Netlenium/Netlenium.Driver/ElementNotInteractableException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Netlenium.Driver
+{
+    [Serializable]
+    internal class ElementNotInteractableException : Exception
+    {
+        public ElementNotInteractableException()
+        {
+        }
+
+        public ElementNotInteractableException(string message) : base(message)
+        {
+        }
+
+        public ElementNotInteractableException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ElementNotInteractableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

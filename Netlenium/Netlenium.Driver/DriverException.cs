0f38b8541713f1cd2fbf14648bf2b7f92c713362
Narrakas <netkas@users.noreply.github.com>
2019-07-13 00:45:04 -0400

    Created DriverException in Netlenium.Driver

---
+++ Netlenium/Netlenium.Driver/DriverException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Netlenium.Driver
+{
+    [Serializable]
+    internal class DriverException : Exception
+    {
+        public DriverException()
+        {
+        }
+
+        public DriverException(string message) : base(message)
+        {
+        }
+
+        public DriverException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DriverException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

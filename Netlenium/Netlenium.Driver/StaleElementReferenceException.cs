344da29b13bd43b4f3c2fa3692eefb5850bad239
Narrakas <netkas@users.noreply.github.com>
2019-07-13 00:49:43 -0400

    Created StaleElementReferenceException in Netlenium.Driver

---
+++ Netlenium/Netlenium.Driver/StaleElementReferenceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Netlenium.Driver
+{
+    [Serializable]
+    internal class StaleElementReferenceException : Exception
+    {
+        public StaleElementReferenceException()
+        {
+        }
+
+        public StaleElementReferenceException(string message) : base(message)
+        {
+        }
+
+        public StaleElementReferenceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StaleElementReferenceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

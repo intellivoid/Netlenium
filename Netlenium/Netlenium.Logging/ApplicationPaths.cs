6a3a5097b384cddc50c2999a06213d7d1f23c5e4
Narrakas <netkas@users.noreply.github.com>
2019-07-14 18:05:06 -0400

    Added ApplicationPaths to Netlenium.Logging

---
+++ Netlenium/Netlenium.Logging/ApplicationPaths.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Netlenium.Logging
+{
+    /// <summary>
+    /// Application Directory Paths that are available
+    /// </summary>
+    public static class ApplicationPaths
+    {
+        public static string AppData
+        {
+            get
+            {
+                var directoryPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}{Path.DirectorySeparatorChar}Netlenium";
+
+                if (Directory.Exists(directoryPath) == false)
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                return directoryPath;
+            }
+        }
+
+        /// <summary>
+        /// Gets the directory dedicated for storing log files
+        /// </summary>
+        public static string LoggingDirectory
+        {
+            get
+            {
+                var directoryPath = $"{AppData}{Path.DirectorySeparatorChar}logs";
+
+                if (Directory.Exists(directoryPath) == false)
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                return directoryPath;
+            }
+        }
+    }
+}

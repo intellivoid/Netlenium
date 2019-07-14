50d0f2f3ce0e4afa2327558f1126237dbe097a02
Narrakas <netkas@users.noreply.github.com>
2019-07-14 14:37:16 -0400

    Created NetleniumServer.Responses.ListWindowHandlesResponse

---
+++ Netlenium/Netlenium Server/Responses/ListWindowHandlesResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response for listing all the Window Handles that are currently available
+    /// </summary>
+    [Serializable]
+    public class ListWindowHandlesResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public List<string> WindowHandles { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="windowHandles"></param>
+        public ListWindowHandlesResponse(List<string> windowHandles)
+        {
+            Status = true;
+            ResponseCode = 200;
+            WindowHandles = windowHandles;
+        }
+    }
+}

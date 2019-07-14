3a5a19fe502b19c52cd6e6964d2808aec67ad89c
Narrakas <netkas@users.noreply.github.com>
2019-07-13 22:41:58 -0400

    Created CurrentWindowResponse in NetleniumServer.Responses

---
+++ Netlenium/Netlenium Server/Responses/CurrentWindowResponse.cs
@@ -0,0 +1,29 @@
+﻿using NetleniumServer.Objects;
+using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given to return the details of the current window
+    /// </summary>
+    [Serializable]
+    public class CurrentWindowResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public CurrentWindow CurrentWindow { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="currentWindow"></param>
+        public CurrentWindowResponse(CurrentWindow currentWindow)
+        {
+            Status = true;
+            ResponseCode = 200;
+            CurrentWindow = currentWindow;
+        }
+    }
+}

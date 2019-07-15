0fd9ffceb3e309e8a7c5b45776861b46b1fe8617
Narrakas <netkas@users.noreply.github.com>
2019-07-15 14:37:02 -0400

    Added Driver and DriverManager to Netlenium.Driver.Firefox (GeckoBuild)

--- Netlenium/Netlenium.Driver/Firefox/Driver.cs
+++ Netlenium/Netlenium.Driver/Firefox/Driver.cs
@@ -1,21 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Netlenium.Logging;
 
 namespace Netlenium.Driver.Firefox
 {
     public class Driver : IDriver
     {
-        public Service Logging => throw new NotImplementedException();
+        private Service logging;
+        public Service Logging => logging;
 
-        public IDriverManager DriverManager => throw new NotImplementedException();
+        private readonly IDriverManager driverManager;
+        public IDriverManager DriverManager { get => driverManager; }
 
-        public Browser TargetBrowser => throw new NotImplementedException();
+        public Browser TargetBrowser => Browser.Firefox;
 
-        public Platform TargetPlatform { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private Platform targetPlatform;
+        public Platform TargetPlatform
+        {
+            get
+            {
+                if (targetPlatform == Platform.AutoDetect)
+                {
+                    logging.WriteEntry(MessageType.Verbose, "Driver", "Auto detecting platform");
+                    targetPlatform = Utilities.DetectPlatform();
+                    logging.WriteEntry(MessageType.Verbose, "Driver", string.Format("Detected Platform: {0}", targetPlatform));
+                }
+
+                return targetPlatform;
+            }
+            set
+            {
+                if (value == Platform.AutoDetect)
+                {
+                    logging.WriteEntry(MessageType.Verbose, "Driver", "Auto detecting platform");
+                    targetPlatform = Utilities.DetectPlatform();
+                    logging.WriteEntry(MessageType.Verbose, "Driver", string.Format("Detected Platform: {0}", targetPlatform));
+                }
+                else
+                {
+                    targetPlatform = value;
+                }
+            }
+        }
 
         public IActions Actions => throw new NotImplementedException();
 
@@ -23,9 +48,38 @@ namespace Netlenium.Driver.Firefox
 
         public IProxy ProxyConfiguration => throw new NotImplementedException();
 
-        public bool Headless { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool DriverLoggingEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool DriverVerboseLoggingEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private bool headless;
+        public bool Headless { get => headless; set => headless = value; }
+
+        private bool driverLoggingEnabled;
+        public bool DriverLoggingEnabled { get => driverLoggingEnabled; set => driverLoggingEnabled = value; }
+
+        private bool driverVerboseLoggingEnabled;
+        public bool DriverVerboseLoggingEnabled { get => driverVerboseLoggingEnabled; set => driverVerboseLoggingEnabled = value; }
+
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public Driver()
+        {
+            logging = new Service()
+            {
+                CommandLineLoggingEnabled = true,
+                DebuggingOutputEnabled = true,
+                InformationEntriesEnabled = true,
+                DebuggingEntriesEnabled = true,
+                ErrorEntriesEnabled = true,
+                VerboseEntriesEnabled = true,
+                WarningEntriesEnabled = true,
+                Name = "Netlenium.Driver.Firefox"
+            };
+
+            driverManager = new DriverManager(this);
+            headless = true;
+            targetPlatform = Platform.AutoDetect;
+        }
+
 
         public void Start()
         {
---
+++ Netlenium/Netlenium.Driver/Firefox/DriverManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netlenium.Driver.Firefox
+{
+    public class DriverManager : IDriverManager
+    {
+        /// <summary>
+        /// The main gecko driver
+        /// </summary>
+        private Driver driver;
+        
+        public bool IsInstalled => throw new NotImplementedException();
+
+        public string DriverExecutableName => throw new NotImplementedException();
+
+        public string DriverDirectoryPath => throw new NotImplementedException();
+
+        public string DriverExecutablePath => throw new NotImplementedException();
+
+        /// <summary>
+        /// Public constructor
+        /// </summary>
+        /// <param name="driver"></param>
+        public DriverManager(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public Version GetCurrentVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Version GetLatestVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initalize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

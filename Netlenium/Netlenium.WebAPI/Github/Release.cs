0ca7f4f2d7114919a8433d32811b2955bdc32743
Narrakas <netkas@users.noreply.github.com>
2019-07-14 23:34:11 -0400

    Created Release in Netlenium.WebAPI.Github

---
+++ Netlenium/Netlenium.WebAPI/Github/Release.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netlenium.WebAPI.Github
+{
+    /// <summary>
+    /// Release Object from GitHub
+    /// </summary>
+    [Serializable]
+    public class Release
+    {
+        public int ID { get; set; }
+
+        public Uri URL { get; set; }
+
+        public Uri HtmlURL { get; set; }
+
+        public Uri AssetsURL { get; set; }
+
+        public Uri UploadURL { get; set; }
+
+        public Uri TarballURL { get; set; }
+
+        public Uri ZipballURL { get; set; }
+
+        public string NodeID { get; set; }
+
+        public string TagName { get; set; }
+
+        public string TargetCommitish { get; set; }
+
+        public string Name { get; set; }
+
+        public string Body { get; set; }
+
+        public bool Draft { get; set; }
+
+        public bool PreRelease { get; set; }
+
+        public string CreatedAt { get; set; }
+
+        public string PublishedAt { get; set; }
+
+        public User Author { get; set; }
+
+        public List<Asset> Assets { get; set; }
+    }
+}

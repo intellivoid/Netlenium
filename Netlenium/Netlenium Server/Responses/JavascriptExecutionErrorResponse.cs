f7db1b4d0f53c38557067e207b744217b5aceb27
Narrakas <netkas@users.noreply.github.com>
2019-07-14 20:30:29 -0400

    Created JavascriptExecutionerroResponse in NetleniumServer.Responses

---
+++ Netlenium/Netlenium Server/Responses/JavascriptExecutionErrorResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given when the Javascript execution failed
+    /// </summary>
+    [Serializable]
+    public class JavascriptExecutionErrorResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string Error { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="exceptionMessage"></param>
+        public JavascriptExecutionErrorResponse(string exceptionMessage)
+        {
+            Status = false;
+            ResponseCode = 500;
+            Error = exceptionMessage;
+        }
+    }
+}

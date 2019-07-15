b7b9a3a1476050289e3135e86fc91b49226c0532
Narrakas <netkas@users.noreply.github.com>
2019-07-14 20:18:09 -0400

    Added JavascriptExecutionResponse to NetleniumServer.Responses

---
+++ Netlenium/Netlenium Server/Responses/JavascriptExecutionResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given after a succesful Javascript Execution
+    /// </summary>
+    [Serializable]
+    public class JavascriptExecutionResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string Output { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="output"></param>
+        public JavascriptExecutionResponse(string output)
+        {
+            Status = true;
+            ResponseCode = 200;
+            Output = output;
+        }
+    }
+}

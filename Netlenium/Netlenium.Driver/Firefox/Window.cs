69afa4e0469ebbab67a692e8c48a05f61d85a4f9
Narrakas <netkas@users.noreply.github.com>
2019-07-16 23:00:10 -0400

    Added Window to Netlenium.Driver.Firefox

---
+++ Netlenium/Netlenium.Driver/Firefox/Window.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netlenium.Driver.Firefox
+{
+    public class Window : IWindow
+    {
+        private string id;
+        public string ID => id;
+
+        private Driver driver;
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="id"></param>
+        public Window(Driver driver, string id)
+        {
+            this.id = id;
+            this.driver = driver;
+        }
+
+        public void SwitchTo()
+        {
+            driver.Logging.WriteEntry(Logging.MessageType.Information, $"Window[{this.id}]", "Switching to this window handle");
+            try
+            {
+                driver.RemoteDriver.SwitchTo().Window(id);
+            }
+            catch (WebDriver.NoSuchWindowException)
+            {
+                driver.Logging.WriteEntry(Logging.MessageType.Error, $"Window[{this.id}]", "Cannot switch to this window handle, no such window exists.");
+                throw new NoSuchWindowException();
+            }
+            catch (Exception exception)
+            {
+                driver.Logging.WriteEntry(Logging.MessageType.Error, $"Window[{this.id}]", $"Cannot switch to this window handle, {exception.Message}");
+                throw exception;
+            }
+        }
+    }
+}

7f5f200f7475166320c185ae74656ccf71eef55b
Narrakas <netkas@users.noreply.github.com>
2019-07-18 03:09:57 -0400

    Updated Netlenium.Driver.Firefox.Actions

--- Netlenium/Netlenium.Driver/Firefox/Actions.cs
+++ Netlenium/Netlenium.Driver/Firefox/Actions.cs
@@ -1,9 +1,6 @@
 ﻿using Netlenium.Driver.WebDriver;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Netlenium.Driver.Firefox
 {

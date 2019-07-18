f117013a719cf6a5698184224c59b0cf5ef518dc
Narrakas <netkas@users.noreply.github.com>
2019-07-18 03:43:27 -0400

    Added NetleniumServer.Responses.DriverDisabledResponse

---
+++ Netlenium/Netlenium Server/Responses/DriverDisabledResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetleniumServer.Responses
+{
+    /// <summary>
+    /// Response given when the requested driver is disabled
+    /// </summary>
+    [Serializable]
+    public class DriverDisabledResponse : IResponse
+    {
+        public bool Status { get; set; }
+
+        public int ResponseCode { get; set; }
+
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="targetBrowser"></param>
+        public DriverDisabledResponse(string targetBrowser)
+        {
+            Status = false;
+            ResponseCode = 403;
+            Message = $"The driver for '{targetBrowser}' is disabled";
+        }
+    }
+}

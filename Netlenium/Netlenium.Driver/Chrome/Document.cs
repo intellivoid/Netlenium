c8cf44dd9b0ccd0d8895fd1585b303dbf86274fc
netkas <netkas@intellivoid.info>
2019-07-18 23:13:17 -0400

    Code cleanup on /Netlenium.Driver/Chrome/Document.c

--- Netlenium/Netlenium.Driver/Chrome/Document.cs
+++ Netlenium/Netlenium.Driver/Chrome/Document.cs
@@ -3,63 +3,68 @@ using System.Collections.ObjectModel;
 
 namespace Netlenium.Driver.Chrome
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Chrome Driver Document
+    /// </summary>
     public class Document : IDocument
     {
-        private Driver driver;
+        private readonly Driver driver;
 
         public Document(Driver driver)
         {
             this.driver = driver;
         }
 
+        /// <inheritdoc />
         public string Title => driver.RemoteDriver.Title;
 
+        /// <inheritdoc />
         public string Uri => driver.RemoteDriver.Url;
 
+        /// <inheritdoc />
         public IWebElement GetElement(SearchBy searchBy, string value)
         {
-            var Results = GetElements(searchBy, value);
+            var results = GetElements(searchBy, value);
 
-            if (Results.Count == 0)
-            {
-                driver.Logging.WriteEntry(Logging.MessageType.Error, "Document", "No elements were found");
-                throw new NoElementsFoundException();
-            }
+            if (results.Count != 0) return results[0];
+            driver.Logging.WriteEntry(Logging.MessageType.Error, "Document", "No elements were found");
+            throw new NoElementsFoundException();
 
-            return Results[0];
         }
 
+        /// <inheritdoc />
         public List<IWebElement> GetElements(SearchBy searchBy, string value)
         {
             driver.Logging.WriteEntry(Logging.MessageType.Information, "Document", $"Searching for '{value}' by '{searchBy}'");
 
-            ReadOnlyCollection<WebDriver.IWebElement> Results;
+            ReadOnlyCollection<WebDriver.IWebElement> results;
 
             switch (searchBy)
             {
 
                 case SearchBy.ClassName:
-                    Results = driver.RemoteDriver.FindElementsByClassName(value);
+                    results = driver.RemoteDriver.FindElementsByClassName(value);
                     break;
 
                 case SearchBy.CssSelector:
-                    Results = driver.RemoteDriver.FindElementsByCssSelector(value);
+                    results = driver.RemoteDriver.FindElementsByCssSelector(value);
                     break;
 
                 case SearchBy.Id:
-                    Results = driver.RemoteDriver.FindElementsById(value);
+                    results = driver.RemoteDriver.FindElementsById(value);
                     break;
 
                 case SearchBy.Name:
-                    Results = driver.RemoteDriver.FindElementsByName(value);
+                    results = driver.RemoteDriver.FindElementsByName(value);
                     break;
 
                 case SearchBy.TagName:
-                    Results = driver.RemoteDriver.FindElementsByTagName(value);
+                    results = driver.RemoteDriver.FindElementsByTagName(value);
                     break;
 
                 case SearchBy.XPath:
-                    Results = driver.RemoteDriver.FindElementsByXPath(value);
+                    results = driver.RemoteDriver.FindElementsByXPath(value);
                     break;
 
                 default:
@@ -67,15 +72,15 @@ namespace Netlenium.Driver.Chrome
                     throw new UnsupportedSearchMethodException();
             }
 
-            var ReturnResults = new List<IWebElement>();
-            foreach (WebDriver.IWebElement webElement in Results)
+            var returnResults = new List<IWebElement>();
+            foreach (var webElement in results)
             {
                 driver.Logging.WriteEntry(Logging.MessageType.Debugging, "Document", webElement.ToString());
-                ReturnResults.Add(new WebElement(driver, webElement));
+                returnResults.Add(new WebElement(driver, webElement));
             }
 
-            driver.Logging.WriteEntry(Logging.MessageType.Verbose, "Document", $"{Results.Count} results were found");
-            return ReturnResults;
+            driver.Logging.WriteEntry(Logging.MessageType.Verbose, "Document", $"{results.Count} results were found");
+            return returnResults;
         }
     }
 }

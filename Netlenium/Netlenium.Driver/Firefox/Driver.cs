1f271fd6190633211aa087151eb06e17ebb798d1
netkas <netkas@intellivoid.info>
2019-07-18 22:59:26 -0400

    Code cleanup on /Netlenium.Driver/Firefox/Driver.c

--- Netlenium/Netlenium.Driver/Firefox/Driver.cs
+++ Netlenium/Netlenium.Driver/Firefox/Driver.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using Netlenium.Driver.WebDriver.Firefox;
 using Netlenium.Driver.WebDriver.Remote;
@@ -7,27 +6,28 @@ using Netlenium.Logging;
 
 namespace Netlenium.Driver.Firefox
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// Firefox Driver
+    /// </summary>
     public class Driver : IDriver
     {
-        private Service logging;
-        public Service Logging => logging;
+        public Service Logging { get; }
 
-        private readonly IDriverManager driverManager;
-        public IDriverManager DriverManager { get => driverManager; }
+        public IDriverManager DriverManager { get; }
 
         public Browser TargetBrowser => Browser.Firefox;
 
         private Platform targetPlatform;
+        
         public Platform TargetPlatform
         {
             get
             {
-                if (targetPlatform == Platform.AutoDetect)
-                {
-                    logging.WriteEntry(MessageType.Verbose, "Driver", "Auto detecting platform");
-                    targetPlatform = Utilities.DetectPlatform();
-                    logging.WriteEntry(MessageType.Verbose, "Driver", string.Format("Detected Platform: {0}", targetPlatform));
-                }
+                if (targetPlatform != Platform.AutoDetect) return targetPlatform;
+                Logging.WriteEntry(MessageType.Verbose, "Driver", "Auto detecting platform");
+                targetPlatform = Utilities.DetectPlatform();
+                Logging.WriteEntry(MessageType.Verbose, "Driver", $"Detected Platform: {targetPlatform}");
 
                 return targetPlatform;
             }
@@ -35,9 +35,9 @@ namespace Netlenium.Driver.Firefox
             {
                 if (value == Platform.AutoDetect)
                 {
-                    logging.WriteEntry(MessageType.Verbose, "Driver", "Auto detecting platform");
+                    Logging.WriteEntry(MessageType.Verbose, "Driver", "Auto detecting platform");
                     targetPlatform = Utilities.DetectPlatform();
-                    logging.WriteEntry(MessageType.Verbose, "Driver", string.Format("Detected Platform: {0}", targetPlatform));
+                    Logging.WriteEntry(MessageType.Verbose, "Driver", $"Detected Platform: {targetPlatform}");
                 }
                 else
                 {
@@ -46,23 +46,20 @@ namespace Netlenium.Driver.Firefox
             }
         }
 
-        private Actions actions;
+        private readonly Actions actions;
         public IActions Actions => actions;
 
-        private Document document;
+        private readonly Document document;
         public IDocument Document => document;
 
-        private Proxy proxyConfiguration;
+        private readonly Proxy proxyConfiguration;
         public IProxy ProxyConfiguration => proxyConfiguration;
 
-        private bool headless;
-        public bool Headless { get => headless; set => headless = value; }
+        public bool Headless { get; set; }
 
-        private bool driverLoggingEnabled;
-        public bool DriverLoggingEnabled { get => driverLoggingEnabled; set => driverLoggingEnabled = value; }
+        public bool DriverLoggingEnabled { get; set; }
 
-        private bool driverVerboseLoggingEnabled;
-        public bool DriverVerboseLoggingEnabled { get => driverVerboseLoggingEnabled; set => driverVerboseLoggingEnabled = value; }
+        public bool DriverVerboseLoggingEnabled { get; set; }
 
         /// <summary>
         /// The Driver Service process manager
@@ -77,19 +74,19 @@ namespace Netlenium.Driver.Firefox
         /// <summary>
         /// Remote Driver Client for controlling the Driver Service
         /// </summary>
-        public RemoteWebDriver RemoteDriver { get; set; }
+        public RemoteWebDriver RemoteDriver { get; private set; }
 
         /// <summary>
         /// The Window size that is set if running in headless mode
         /// </summary>
-        private Size HeadlessWindowSize { get; set; }
+        private Size HeadlessWindowSize { get; }
 
         /// <summary>
         /// Public Constructor
         /// </summary>
         public Driver()
         {
-            logging = new Service()
+            Logging = new Service
             {
                 CommandLineLoggingEnabled = true,
                 FileLoggingEnabled = true,
@@ -104,11 +101,11 @@ namespace Netlenium.Driver.Firefox
 
             HeadlessWindowSize = new Size(1920, 1080);
 
-            driverManager = new DriverManager(this);
+            DriverManager = new DriverManager(this);
             actions = new Actions(this);
             document = new Document(this);
             proxyConfiguration = new Proxy();
-            headless = true;
+            Headless = true;
             targetPlatform = Platform.AutoDetect;
         }
 
@@ -121,9 +118,9 @@ namespace Netlenium.Driver.Firefox
         {
             foreach (var option in options)
             {
-                var Argument = option.Value == string.Empty ? $"-{option.Key}" : $"-{option.Key}={option.Value}";
-                logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting argument '{Argument}'");
-                DriverOptions.AddArgument(Argument);
+                var argument = option.Value == string.Empty ? $"-{option.Key}" : $"-{option.Key}={option.Value}";
+                Logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting argument '{argument}'");
+                DriverOptions.AddArgument(argument);
             }
         }
 
@@ -131,103 +128,95 @@ namespace Netlenium.Driver.Firefox
         {
             if (DriverService != null)
             {
-                if (DriverService.IsRunning == true)
+                if (DriverService.IsRunning)
                 {
-                    logging.WriteEntry(MessageType.Error, "Driver", "Remote Driver Service cannot be stopped because it is not running");
+                    Logging.WriteEntry(MessageType.Error, "Driver", "Remote Driver Service cannot be stopped because it is not running");
                     throw new DriverAlreadyRunningException();
                 }
             }
 
             DriverManager.Initalize();
 
-            logging.WriteEntry(MessageType.Information, "Driver", "Starting remote driver service");
+            Logging.WriteEntry(MessageType.Information, "Driver", "Starting remote driver service");
 
-            logging.WriteEntry(MessageType.Debugging, "Driver", $"Working Directory: {DriverManager.DriverDirectoryPath}");
-            logging.WriteEntry(MessageType.Debugging, "Driver", $"Driver Executable: {DriverManager.DriverExecutableName}");
+            Logging.WriteEntry(MessageType.Debugging, "Driver", $"Working Directory: {DriverManager.DriverDirectoryPath}");
+            Logging.WriteEntry(MessageType.Debugging, "Driver", $"Driver Executable: {DriverManager.DriverExecutableName}");
             DriverService = FirefoxDriverService.CreateDefaultService(DriverManager.DriverDirectoryPath, DriverManager.DriverExecutableName);
-            logging.WriteEntry(MessageType.Debugging, "Driver", "DriverService Constructed");
+            Logging.WriteEntry(MessageType.Debugging, "Driver", "DriverService Constructed");
 
 
-            logging.WriteEntry(MessageType.Verbose, "Driver", "Creating options for driver");
+            Logging.WriteEntry(MessageType.Verbose, "Driver", "Creating options for driver");
             DriverOptions = new FirefoxOptions();
 
             var options = new Dictionary<string, string>();
 
-            if (Headless == true)
+            if (Headless)
             {
-                logging.WriteEntry(MessageType.Debugging, "Driver", $"Headless mode with window-size of {HeadlessWindowSize.Width}x{HeadlessWindowSize.Height}");
+                Logging.WriteEntry(MessageType.Debugging, "Driver", $"Headless mode with window-size of {HeadlessWindowSize.Width}x{HeadlessWindowSize.Height}");
                 options.Add("headless", string.Empty);
                 options.Add("window-size", $"{HeadlessWindowSize.Width}x{HeadlessWindowSize.Height}");
                 DriverOptions.SetPreference("media.volume_scale", "0.0");
             }
             
-            if (proxyConfiguration.Enabled == true)
+            if (proxyConfiguration.Enabled)
             {
-                logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting HTTP Proxy {string.Format("{0}:{1}", proxyConfiguration.Host, proxyConfiguration.Port)}");
-                DriverOptions.Proxy.HttpProxy = string.Format("{0}:{1}", proxyConfiguration.Host, proxyConfiguration.Port);
-                logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting FTP Proxy {string.Format("{0}:{1}", proxyConfiguration.Host, proxyConfiguration.Port)}");
-                DriverOptions.Proxy.FtpProxy = string.Format("{0}:{1}", proxyConfiguration.Host, proxyConfiguration.Port);
-                logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting SOCKS Proxy {string.Format("{0}:{1}", proxyConfiguration.Host, proxyConfiguration.Port)}");
-                DriverOptions.Proxy.SocksProxy = string.Format("{0}:{1}", proxyConfiguration.Host, proxyConfiguration.Port);
+                Logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting HTTP Proxy {proxyConfiguration.Host}:{proxyConfiguration.Port}");
+                DriverOptions.Proxy.HttpProxy = $"{proxyConfiguration.Host}:{proxyConfiguration.Port}";
+                Logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting FTP Proxy {proxyConfiguration.Host}:{proxyConfiguration.Port}");
+                DriverOptions.Proxy.FtpProxy = $"{proxyConfiguration.Host}:{proxyConfiguration.Port}";
+                Logging.WriteEntry(MessageType.Debugging, "Driver", $"Setting SOCKS Proxy {proxyConfiguration.Host}:{proxyConfiguration.Port}");
+                DriverOptions.Proxy.SocksProxy = $"{proxyConfiguration.Host}:{proxyConfiguration.Port}";
 
-                if (proxyConfiguration.AuthenticationRequried == true)
+                if (proxyConfiguration.AuthenticationRequried)
                 {
-                    logging.WriteEntry(MessageType.Debugging, "Driver", "Setting SOCKS Proxy authentication");
+                    Logging.WriteEntry(MessageType.Debugging, "Driver", "Setting SOCKS Proxy authentication");
                     DriverOptions.Proxy.SocksUserName = proxyConfiguration.Username;
                     DriverOptions.Proxy.SocksPassword = proxyConfiguration.Password;
                 }
             }
 
             DriverService.HideCommandPromptWindow = false;
-            var DriverCommandLineOptions = string.Empty;
-            if (driverLoggingEnabled == false)
+            string driverCommandLineOptions;
+            if (DriverLoggingEnabled == false)
             {
-                DriverCommandLineOptions = "--log fatal";
+                driverCommandLineOptions = "--log fatal";
                 DriverService.SuppressInitialDiagnosticInformation = true;
             }
             else
             {
-                if (driverVerboseLoggingEnabled == true)
-                {
-                    DriverCommandLineOptions = "--log debug";
-                }
-                else
-                {
-                    DriverCommandLineOptions = "--log info";
-                }
-
+                driverCommandLineOptions = DriverVerboseLoggingEnabled ? "--log debug" : "--log info";
                 DriverService.SuppressInitialDiagnosticInformation = false;
             }
 
-            logging.WriteEntry(MessageType.Debugging, "Driver", $"Using arguments for driver '{DriverCommandLineOptions}'");
-            logging.WriteEntry(MessageType.Verbose, "Driver", "Setting options for driver");
+            Logging.WriteEntry(MessageType.Debugging, "Driver", $"Using arguments for driver '{driverCommandLineOptions}'");
+            Logging.WriteEntry(MessageType.Verbose, "Driver", "Setting options for driver");
             SetOptions(options);
 
-            logging.WriteEntry(MessageType.Verbose, "Driver", "Starting Driver Service");
-            DriverService.Start(DriverCommandLineOptions);
+            Logging.WriteEntry(MessageType.Verbose, "Driver", "Starting Driver Service");
+            DriverService.Start(driverCommandLineOptions);
 
-            logging.WriteEntry(MessageType.Debugging, "Driver", $"Service URL: {DriverService.ServiceUrl}");
-            logging.WriteEntry(MessageType.Debugging, "Driver", $"Process ID: {DriverService.ProcessId}");
-            logging.WriteEntry(MessageType.Debugging, "Driver", $"Port: {DriverService.Port}");
+            Logging.WriteEntry(MessageType.Debugging, "Driver", $"Service URL: {DriverService.ServiceUrl}");
+            Logging.WriteEntry(MessageType.Debugging, "Driver", $"Process ID: {DriverService.ProcessId}");
+            Logging.WriteEntry(MessageType.Debugging, "Driver", $"Port: {DriverService.Port}");
 
-            logging.WriteEntry(MessageType.Verbose, "Driver", $"Connecting to '{DriverService.ServiceUrl}'");
+            Logging.WriteEntry(MessageType.Verbose, "Driver", $"Connecting to '{DriverService.ServiceUrl}'");
             RemoteDriver = new RemoteWebDriver(DriverService.ServiceUrl, DriverOptions);
 
-            logging.WriteEntry(MessageType.Information, "Driver", "Remote Driver Serivce Started");
+            Logging.WriteEntry(MessageType.Information, "Driver", "Remote Driver Service Started");
         }
 
         public void Stop()
         {
             if (DriverService != null)
             {
-                logging.WriteEntry(MessageType.Information, "Driver", "Quitting remote driver");
+                Logging.WriteEntry(MessageType.Information, "Driver", "Quitting remote driver");
                 RemoteDriver.Quit();
-                logging.WriteEntry(MessageType.Verbose, "Driver", "Disposing Driver Service");
+                Logging.WriteEntry(MessageType.Verbose, "Driver", "Disposing Driver Service");
                 DriverService.Dispose();
-                logging.WriteEntry(MessageType.Information, "Driver", "Remote Driver Service stopped");
+                Logging.WriteEntry(MessageType.Information, "Driver", "Remote Driver Service stopped");
             }
 
-            logging.WriteEntry(MessageType.Error, "Driver", "Remote Driver Service cannot be stopped because it is not running");
+            Logging.WriteEntry(MessageType.Error, "Driver", "Remote Driver Service cannot be stopped because it is not running");
             throw new DriverNotRunningException();
         }
     }

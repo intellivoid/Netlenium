7f33f14181f4efb722a41a9035b5b2fac112ac65
netkas <netkas@intellivoid.info>
2019-07-18 23:46:03 -0400

    Removed redundant code from Netlenium.WebAPI.Google

--- Netlenium/Netlenium.WebAPI/Google/Storage.cs
+++ Netlenium/Netlenium.WebAPI/Google/Storage.cs
@@ -24,7 +24,7 @@ namespace Netlenium.WebAPI.Google
 
             foreach (XmlNode contentNode in xmlDocument.GetElementsByTagName("Contents"))
             {
-                contentResults.Add(new Content()
+                contentResults.Add(new Content
                 {
                     Name = contentNode["Key"].InnerText,
                     Generation = contentNode["Generation"].InnerText,
